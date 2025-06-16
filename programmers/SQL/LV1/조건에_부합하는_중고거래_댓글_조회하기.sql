782aceb87c3d48c2d7486b06045154f59132e133
archive master <onlybussinessmail@gmail.com>
2025-06-16 18:51:14 +0900

    Create 조건에_부합하는_중고거래_댓글_조회하기.sql

---
+++ programmers/SQL/LV1/조건에_부합하는_중고거래_댓글_조회하기.sql
@@ -0,0 +1,26 @@
+--문제: USED_GOODS_BOARD와 USED_GOODS_REPLY 테이블에서 2022년 10월에 작성된 게시글 제목, 게시글 ID, 댓글 ID, 댓글 작성자 ID, 댓글 내용, 댓글 작성일을 조회하는 SQL문을 작성해주세요. 결과는 댓글 작성일을 기준으로 오름차순 정렬해주시고, 댓글 작성일이 같다면 게시글 제목을 기준으로 오름차순 정렬해주세요.
+--출처: https://school.programmers.co.kr/learn/courses/30/lessons/164673
+
+--오답1
+select  b.TITLE,
+     b.BOARD_ID,
+     r.REPLY_ID,
+     r.WRITER_ID,
+     r.CONTENTS,
+     Date_format(r.CREATED_DATE, "%Y-%m-%d") as CREATED_DATE
+ from USED_GOODS_BOARD b, USED_GOODS_REPLY r
+ where b.BOARD_ID = r.BOARD_ID and Date_format(r.CREATED_DATE, "%Y-%m") = '2022-10'
+ order by r.CREATED_DATE asc, b.TITLE asc
+-- 쿼리 결과는 똑같지만 where조건에서 ='2022-10'조건으로 적어서 오답으로 인식함
+
+--정답
+select  b.TITLE,
+    b.BOARD_ID,
+    r.REPLY_ID,
+    r.WRITER_ID,
+    r.CONTENTS,
+    Date_format(r.CREATED_DATE, "%Y-%m-%d") as CREATED_DATE
+from USED_GOODS_BOARD b, USED_GOODS_REPLY r
+where b.BOARD_ID = r.BOARD_ID and b.CREATED_DATE between '2022-10-01' and '2022-10-31'
+order by r.CREATED_DATE asc, b.TITLE asc
+-- between 조건으로 변경하면 정답처리 
