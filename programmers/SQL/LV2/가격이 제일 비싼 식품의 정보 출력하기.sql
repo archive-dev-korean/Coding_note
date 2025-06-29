6bbb2031aee3a43152c9eee86f4ae155a10b9a02
archive master <onlybussinessmail@gmail.com>
2025-06-29 16:45:56 +0900

    Create 가격이 제일 비싼 식품의 정보 출력하기.sql

---
+++ programmers/SQL/LV2/가격이 제일 비싼 식품의 정보 출력하기.sql
@@ -0,0 +1,7 @@
+--문제 : FOOD_PRODUCT 테이블에서 가격이 제일 비싼 식품의 식품 ID, 식품 이름, 식품 코드, 식품분류, 식품 가격을 조회하는 SQL문을 작성해주세요.
+--출처 : https://school.programmers.co.kr/learn/courses/30/lessons/131115
+
+SELECT *
+from FOOD_PRODUCT 
+order by price desc
+limit 1
