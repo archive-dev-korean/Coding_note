5140c5520a4bee2577882f4962be92d24b9a0a46
archive master <onlybussinessmail@gmail.com>
2025-08-31 17:21:02 +0900

    Update 4_대표값.py

--- 알고리즘 학습/기본구현/4_대표값.py
+++ 알고리즘 학습/기본구현/4_대표값.py
@@ -16,3 +16,25 @@ for i in range(N):
 
     # student[i]=student[i]
 print(avg, best_idx + 1)
+
+
+
+# 하지만 원래 정답은 조금 더 쉽게 구할 수 있음(보기좋게)
+n=int(input())
+a=list(map(int, input().split()))
+ave=round(sum(a)/n)
+min=123331233131
+for idx, x in enumerate(a):
+  tmp=abs(x-ave)
+  if tmp<min:
+    min=tmp
+    score=x
+    res=idx+1
+  elif tmp==min:
+    if x>score:
+      score=x
+      res=idx+1
+print(ave, res)
+
+# 조건문을 따지고 들어가면 tmp가 평균과 원소 값 차이의 절댓값을 나타내는 변수이고 이것도 최솟값을 구하는 알고리즘 처럼 가장 작은 수를 구하게 됨
+# 그리고 만약 차이가 같아면 점수가 더 큰 수를 출력하고 그마저 같다면 원소의 순서를 출력하는 로직임
