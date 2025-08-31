1dead2f5f9424cf5000f0ac2d56d6c4a389a2744
archive master <onlybussinessmail@gmail.com>
2025-08-31 18:34:08 +0900

    Update 5_정다면체.py

--- 알고리즘 학습/기본구현/5_정다면체.py
+++ 알고리즘 학습/기본구현/5_정다면체.py
@@ -20,3 +20,20 @@ modes.sort()
 print(*modes)
 
 # 일단 내 방식대로 구했는데 빈도수 세는 거는 gpt도움 받았다.
+
+
+n,m=map(int, input().split())
+cnt=[0]*(n+m+1) # Increased size by 1
+max=0
+for i in range(1,n+1):
+  for j in range(1,m+1):
+    cnt[i+j] += 1
+for i in range(n+m+1):
+  if cnt[i]>max:
+    max=cnt[i]
+for i in range(n+m+1):
+  if cnt[i]==max:
+    print(i, end=' ') # Added a space for better output
+
+# 이게 정답 코드임 
+# 딕셔너리 안쓰고도 구할 수 있음
