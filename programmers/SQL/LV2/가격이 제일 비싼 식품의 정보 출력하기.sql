16a74f72a76c6002f61b55d4a64193ba9c8780d9
archive master <onlybussinessmail@gmail.com>
2025-09-10 02:26:53 +0900

    Update 가격이 제일 비싼 식품의 정보 출력하기.sql

--- programmers/SQL/LV2/가격이 제일 비싼 식품의 정보 출력하기.sql
+++ programmers/SQL/LV2/가격이 제일 비싼 식품의 정보 출력하기.sql
@@ -5,3 +5,8 @@ SELECT *
 from FOOD_PRODUCT 
 order by price desc
 limit 1
+
+-- max를 이용한 풀이
+select PRODUCT_ID, PRODUCT_NAME, PRODUCT_CD, CATEGORY, PRICE
+from FOOD_PRODUCT
+where price =(select max(price) from FOOD_PRODUCT)
