aac2636449083da80a927942f1bd47db38d4ae31
archive-dev-master <onlybussinessmail@gmail.com>
2026-01-31 22:37:17 +0900

    심심풀이 폴더에 브론즈 문제 몇개 추가

---
+++ 백준/심심풀이/10809_알파벳 찾기.py
@@ -0,0 +1,11 @@
+# 연습
+# S=list(input())
+S=input()
+# d=dict()
+a=[]
+b=[]
+a.extend(S)
+print(a)
+for i in a:
+    b.append(i)
+print(b)
---
+++ 백준/심심풀이/11720_숫자의 합.py
@@ -0,0 +1,14 @@
+# 브론즈 난이도
+N=int(input())
+a=list(input())
+s=0
+print(a)
+# i=list(input())
+# a.append(i)
+# print(a)
+# for i in a:
+#     s +=int(i)
+# print(s)
+for j in a:
+    s += int(j)
+print(s)
