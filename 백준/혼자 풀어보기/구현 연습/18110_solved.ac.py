7a2fb89ef194a99bbbeba4efdf6f760e547e425b
archive-dev-master <onlybussinessmail@gmail.com>
2026-01-31 18:24:47 +0900

    18110 반올림 문제 round써도 다른덴 정답 가능 한 것 같은데

---
+++ 백준/혼자 풀어보기/구현 연습/18110_solved.ac.py
@@ -0,0 +1,28 @@
+# 구현 연습
+import sys
+input=sys.stdin.readline
+n=int(input())
+l=[]
+for _ in range(n):
+    i=int(input())
+    l.append(i)
+# print(sorted(l))
+l=sorted(l)
+#l.sort() # 이렇게 써도 됨
+# 반올림함수 만들어서 round를 그걸로 교체하기
+def r(x):
+    return int(x+0.5)
+a15=r(len(l) * 0.15) #여기서 에러 나는 것 같은데
+# print(a15)
+avg=0
+result=l[a15:n-a15]
+# for i in range(a15 -1):
+#     if i == 1:
+#         result=l[1:]
+#     else:
+#         result=i[i-1:]
+if len(result) ==0:
+    avg=0
+else:
+    avg=r(sum(result)/len(result))
+print(avg)
