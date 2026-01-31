94542c3bc73d1aa5e55c99638d1033928d1d05a7
archive-dev-master <onlybussinessmail@gmail.com>
2026-01-31 18:27:56 +0900

    18110 주석 수정

--- 백준/혼자 풀어보기/구현 연습/18110_solved.ac.py
+++ 백준/혼자 풀어보기/구현 연습/18110_solved.ac.py
@@ -12,7 +12,8 @@ l=sorted(l)
 # 반올림함수 만들어서 round를 그걸로 교체하기
 def r(x):
     return int(x+0.5)
-a15=r(len(l) * 0.15) #여기서 에러 나는 것 같은데
+# a15=round(len(l) * 0.15) #여기서 에러 나는 것 같은데
+a15=r(len(l) * 0.15) #round 대신 r 함수 적용
 # print(a15)
 avg=0
 result=l[a15:n-a15]
