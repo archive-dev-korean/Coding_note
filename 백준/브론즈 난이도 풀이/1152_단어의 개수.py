7e2c4c7b099433fe9e3ff502bc5798fb670cdff3
archive-dev-master <onlybussinessmail@gmail.com>
2026-02-02 20:18:37 +0900

    1152 단어의 개수 풀이 어렵게 품

---
+++ 백준/브론즈 난이도 풀이/1152_단어의 개수.py
@@ -0,0 +1,32 @@
+# # 연습
+# import sys
+# input=sys.stdin.readline
+# N=input().strip()
+# a=[]
+# cnt=0
+# # a.append(N)
+# # print(a)
+# if N=='':
+#     print(0)
+# else:
+#     for i in N:
+#         # print(i)
+#         if i==' ':
+#             cnt+=1
+#         else:
+#             cnt
+#     # elif a[-1] ==' ':
+#     #     cnt-=1
+#     # elif i == ' ':
+#     #     cnt+=1
+#     print(cnt+1)
+import sys
+input=sys.stdin.readline
+N=input().split()
+# N=input().strip()
+a=[]
+a.extend(N)
+print(N)
+print(a)
+print(len(N))
+print(len(a))
