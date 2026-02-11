064c341b26edef610c5b0479b5808db22a57a1b9
archive-dev-master <onlybussinessmail@gmail.com>
2026-02-11 19:51:18 +0900

    10250 문제 풀이

--- 백준/브론즈 난이도 풀이/10250_ACM 호텔.py
+++ 백준/브론즈 난이도 풀이/10250_ACM 호텔.py
@@ -1,12 +1,21 @@
 # 연습
 T=int(input())
-arr=[]
 for _ in range(T):
+    arr=[]
     a,b,c=map(int,input().split())
-for i in range(1,a+1):
-    for j in range(1,b+1):
-        m=j//10
-        n=j%10
-        if i==1:
-            arr.append(1,m,n)
-        print(arr)
+    for j in range(1,b+1): # 호수 1호,2호...
+        for i in range(1,a+1): # 층수 1층 2층..
+            arr.append((i,j)) # (층,호수) -> 각층 1호 부터 차근차근 넣어짐
+    # print(arr)
+    floor,room=arr[c-1]
+    # print(f"{floor}{room:02d}")
+    # f 스트링 옵션 인데
+    # 0 -> 빈자리 0으로 채워라
+    # 2 -> 최소 2자리로 만들어라
+    # d -> 정수
+
+    # 또는
+    if room < 10:
+        print(str(floor) + "0" + str(room))
+    else:
+        print(str(floor) + str(room))
