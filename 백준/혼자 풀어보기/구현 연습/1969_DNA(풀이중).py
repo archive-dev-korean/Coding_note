8a9511e80d2f54732d30e9c0751dda96a16a76f2
archive-dev-master <onlybussinessmail@gmail.com>
2026-02-20 16:54:40 +0900

    1969 풀이중

---
+++ 백준/혼자 풀어보기/구현 연습/1969_DNA(풀이중).py
@@ -0,0 +1,17 @@
+# 구현 연습
+N,M=map(int,input().split())
+
+for _ in range(N):
+    i=input()
+DNA=['A','T','G','C']
+s=[0] * M
+# for _ in range(M):
+#     for i in DNA:
+#         s.append(i)
+# # s=[0] * M
+if s[0]=='A':
+    for j in i:
+        j!=s[0]
+print(s)
+
+# 이거는 각 열에서 가장 많은 문자를 세서 그게 s로 최종나와야 함
