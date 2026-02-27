f0766e2470e27967ee9defe6229e6435e14775c0
archive-dev-master <onlybussinessmail@gmail.com>
2026-02-27 13:48:29 +0900

    2217번 풀이

--- 백준/혼자 풀어보기/그리디 연습/2217_로프(풀이중).py
+++ 백준/혼자 풀어보기/그리디 연습/2217_로프(풀이중).py
@@ -1,11 +1,22 @@
 # 그리디 연습
 N=int(input())
 l=[]
+result=[]
 for _ in range(N):
     i=int(input())
     l.append(i)
-l.sort(reverse=True)
+l.sort()
 # print(l[0]*N)
 
-print(l)
-#핵심 아이디어 로프를 내림차순으로 정렬후 i,i+1 곱해보기
+# print(l)
+#핵심 아이디어 로프를 내림차순으로 정렬후 i,i+1 곱해보기 -> GPT
+# for i in range(1,N+1):
+#     print(l[0]*i)
+# print(l[0]*N)
+# print(l)
+for i in range(N):
+    result.append(l[i]*(N-i))
+    # N-=1
+# result.sort(reverse=True)
+# print(result[0])
+print(max(result))
