40d9626463aa49885d1d3b7e78a625eb219e504f
archive-dev-master <onlybussinessmail@gmail.com>
2026-03-02 14:54:47 +0900

    1406 풀이 했지만 도움 많이 받음

--- 백준/혼자 풀어보기/자료구조/스택/1406_에디터(개인적으로 어려움).py
+++ 백준/혼자 풀어보기/자료구조/스택/1406_에디터(개인적으로 어려움).py
@@ -8,7 +8,7 @@ l=[] #결과값을 담을 리스트
 # # cursor=len(N)-1
 # # l.append(N)
 for _ in range(M):
-    cmd=map(str,input().split())
+    cmd=input().split()
 #     if i=='L':
 #         cursor-=1
 #         if cursor==0:
@@ -27,12 +27,22 @@ for _ in range(M):
     if cmd[0]=='L':
         if len(N)>0:
             l.append(N.pop())
-        elif cmd[0]=='D':
-            if len(l)>0:
-                l.append(l.pop())
-        elif cmd[0]=='B':
-            if len(N)>0:
-                N.pop()
-        elif cmd[0]=='P':
-            N.append(cmd[1])
-print(N+l[::-1])
+        else:
+            continue
+    elif cmd[0]=='D':
+        if len(l)>0:
+            N.append(l.pop())
+        else:
+            continue
+    elif cmd[0]=='B':
+        if len(N)>0:
+            N.pop()
+        else:
+            continue
+    elif cmd[0]=='P':
+        N.append(cmd[1])
+print(*(N+l[::-1]),sep='')
+
+# POP 을 이용해서 새로운 리스트에 값을 담거나 기존 리스트에 값을 추가하면서 푸는 문제임
+# 스택 2개를 이용해서 푸는거임
+# >>> 제발 이런 생각좀 해봐라 진짜.
