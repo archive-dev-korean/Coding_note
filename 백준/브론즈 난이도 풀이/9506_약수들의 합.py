30a6bc5f6402dc3350a16290802e6d3e7120b094
archive-dev-master <onlybussinessmail@gmail.com>
2026-03-15 18:42:56 +0900

    9506 math 풀이

--- 백준/브론즈 난이도 풀이/9506_약수들의 합.py
+++ 백준/브론즈 난이도 풀이/9506_약수들의 합.py
@@ -17,3 +17,25 @@ while True:
                 #     print(f"{i}="+result[0]+"{result[y]}")
         else:
             print(f"{i} is NOT perfect.")
+# 근데 더 빨리 풀수 있는 방법은 루트를 쓰는 방법임
+import math
+
+while True:
+    n = int(input())
+    if n == -1:
+        break
+
+    div = [1]
+
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            div.append(i)
+            if i != n // i:
+                div.append(n // i)
+
+    div.sort()
+
+    if sum(div) == n:
+        print(f"{n} = " + " + ".join(map(str, div)))
+    else:
+        print(f"{n} is NOT perfect")
