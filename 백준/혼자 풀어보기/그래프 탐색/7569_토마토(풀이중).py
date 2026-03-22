c2a5cf63d02885749000889113d38167bd8555eb
archive-dev-master <onlybussinessmail@gmail.com>
2026-03-22 18:35:58 +0900

    7569 풀이중

--- 백준/혼자 풀어보기/그래프 탐색/7569_토마토(풀이중).py
+++ 백준/혼자 풀어보기/그래프 탐색/7569_토마토(풀이중).py
@@ -1,3 +1,15 @@
 # 그래프 연습(골드5)
 # BFS로 푸는 거겠지??
 # V,E=map(int,input().split())
+from collections import deque
+M,N,H=map(int,input().split())
+for _ in range(N):
+    for _ in range(H):
+        V=list(map(int,input().split()))
+        # print(type(V))
+def bfs(now,nxt):
+    d=deque()
+    d.append(now)
+    while d:
+        now=d.popleft()
+        for nxt in range(1,M+1):
